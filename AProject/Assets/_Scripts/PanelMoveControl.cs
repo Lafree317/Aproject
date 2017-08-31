5d8e63014d41a0524e51cc2880c04666e4835dd9
LafreeZhaZha <lafree317@163.com>
2017-08-31 23:14:56 +0800

    复杂动画开发

---
+++ AProject/Assets/_Scripts/PanelMoveControl.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * 
+ *      Title:声音设置面板移动
+ * 
+ *      Description:
+ * 
+ *      Date:
+ * 
+ *      Version:
+ * 
+ *      Modify Recoder:
+ * 
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMoveControl : MonoBehaviour {
+    public Animator Ani_AudioSettingPanel; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DisplaySettingPanel() {
+        Ani_AudioSettingPanel.SetBool("isDisPlayInfo", true);
+    }
+
+    public void HideAuidoSettingPanel() {
+        Ani_AudioSettingPanel.SetBool("isDisPlayInfo", false);
+    }
+}
--- AProject/Assets/_Scripts/StartLevel/StartPanelMovingControl.cs
+++ AProject/Assets/_Scripts/StartLevel/StartPanelMovingControl.cs
@@ -25,7 +25,7 @@ public class StartPanelMovingControl : MonoBehaviour,IBeginDragHandler,IDragHand
     private float _FloStartPositionX; // 鼠标开始的偏移量
     private float _FloPosX;  // 鼠标移动的偏移量
     private float _FloSliderSpeed = 1000;
-    private float _FloSmooth = 10F;
+    private float _FloSmooth = 3F;
 	// Use this for initialization
 	void Start () {
 		
@@ -33,14 +33,23 @@ public class StartPanelMovingControl : MonoBehaviour,IBeginDragHandler,IDragHand
 
     // Update is called once per frame
     void Update () {
-        // 滑动脚本完善
+        // 滑动脚本完善 添加边界
         print(this.transform.localPosition.x);
-        if(this.transform.localPosition.x > 0) {
+        
+        if(this.transform.localPosition.x > 0  ) {
             this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, new Vector3(0, 0, 0), Time.deltaTime * _FloSmooth);
         }else if(this.transform.localPosition.x < -3200) {
             this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, new Vector3(-3200, 0, 0), Time.deltaTime * _FloSmooth);
+        }else if(this.transform.localPosition.x <0 && this.transform.localPosition.x >= -800) {
+            this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, new Vector3(-800, 0, 0), Time.deltaTime * _FloSmooth);
+        }else if(this.transform.localPosition.x < -800 && this.transform.localPosition.x >= -1600) {
+            this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, new Vector3(-1600, 0, 0), Time.deltaTime * _FloSmooth);
+        }else if(this.transform.localPosition.x < -1600 && this.transform.localPosition.x >= -2400) {
+            this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, new Vector3(-2400, 0, 0), Time.deltaTime * _FloSmooth);
+        } else if (this.transform.localPosition.x < -2400 && this.transform.localPosition.x >= -3200) {
+            this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, new Vector3(-3200, 0, 0), Time.deltaTime * _FloSmooth);
         }
-        
+
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
@@ -52,19 +61,11 @@ public class StartPanelMovingControl : MonoBehaviour,IBeginDragHandler,IDragHand
         
         _FloPosX = eventData.position.x - _FloStartPositionX;
         float changeSpeed = _FloPosX / _FloSliderSpeed;
-        //if ((this.transform.localPosition.x + changeSpeed) >= 0 && _FloPosX > 0) {
-        //    this.transform.localPosition = new Vector3(0, 0, 0);
-        //    return;
-        //}
 
-        //if((this.transform.localPosition.x + changeSpeed) <= -3200 && _FloPosX < 0) {
-        //    this.transform.localPosition = new Vector3(-3200, 0, 0);
-        //    return;
-        //}
-        
+        // 移动面板
         this.transform.Translate(new Vector3(changeSpeed, 0, 0), Space.World);
         
-        // 移动面板
+        
         
         
     }
